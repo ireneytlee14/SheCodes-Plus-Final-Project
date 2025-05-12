dc583b97735ffce973826979da8597badf2ce8ad
ireneytlee14 <54793680+ireneytlee14@users.noreply.github.com>
2025-05-12 20:00:12 +0200

    updated forecast

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
 iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -32,13 +34,57 @@ let days = [
 ];
 let day = days[date.getDay()];
 
-if (minutes < 10) { minutes=`0${minutes}`; } return `${day} ${hours}:${minutes}`; } function searchCity(city){ let
-    apiKey="bbbda6fa62tff039o9e02b3f44a6b205" ; let
-    apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(refreshWeather); } function handleSearchSubmit(event){ event.preventDefault(); let
-    searchInput=document.querySelector("#search-form-input"); let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value; searchCity(searchInput.value); } let
-    searchFormElement=document.querySelector("#search-form"); searchFormElement.addEventListener("submit",
-    handleSearchSubmit); searchCity("San Jose");
+if (minutes < 10) { minutes=`0${minutes}`; 
+} 
+return `${day} ${hours}:${minutes}`; 
+} 
 
+function searchCity(city) {
+  let apiKey = "bbbda6fa62tff039o9e02b3f44a6b205";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+function getForecast(city){
+    let apiKey = "bbbda6fa62tff039o9e02b3f44a6b205";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
 
